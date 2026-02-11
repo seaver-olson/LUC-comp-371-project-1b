56dd5b543a159d5cc7c88ca7d49a85ed16990276
Seaver Olson <sjolson007@gmail.com>
2026-02-11 10:30:14 -0600

    added basic setup

---
+++ topwords/src/main/scala/edu/luc/cs/cs371/topwords/TopWordsProcessor.scala
@@ -0,0 +1,17 @@
+package edu.luc.cs.cs371.topwords
+
+class TopWordsProcessor(
+  cloudSize: Int,
+  minLength: Int,
+  windowSize: Int,
+  observer: WordCloudObserver
+  ):
+  private val window 
+  private vval freqs
+  private var wordCount = 0
+
+  def processWord(word: String): Unit =
+    ()
+
+  private def generateWordCloud(): List[(String, Int)] =
+    List.empty
